c065cf48b80f8f53477b30a4522b6748827d34cf
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-05-22 17:19:51 +0300

    G

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -20,7 +20,7 @@ module.exports = {
 
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'distt/'),
-    publicPath:'www.my-amazing-budget-app.netlify.com/'
+    publicPath:'/'
 
   },
   mode: 'production',
