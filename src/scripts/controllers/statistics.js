216025395941579d7f23bfa951cb7e52485f5f79
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-05-24 18:54:32 +0300

    targeted chrome browser in mobile devices to tackle the nav bar width issue

--- src/scripts/controllers/statistics.js
+++ src/scripts/controllers/statistics.js
@@ -8,3 +8,9 @@ if (document.querySelector('body').className === 'statistics') {
   document.querySelector('.js-monthly').addEventListener('click', stats.monthly);
   document.querySelector('.js-yearly').addEventListener('click', stats.yearly);
 }
+
+const isChromium = !!window.chrome;
+if(isChromium &&  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    console.log('hellooooo')
+  document.querySelector('.container').style.maxHeight="calc(100vh - 56px)";
+}
