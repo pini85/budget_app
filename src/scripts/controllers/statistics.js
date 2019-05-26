af4ffdb96f805315648dbcc7c40a9f1ebc58eb12
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-05-26 12:02:57 +0300

    g

--- src/scripts/controllers/statistics.js
+++ src/scripts/controllers/statistics.js
@@ -12,5 +12,20 @@ if (document.querySelector('body').className === 'statistics') {
 const isChromium = !!window.chrome;
 if(isChromium &&  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent &&  document.querySelector('.container') )){
     console.log('hellooooo')
-  document.querySelector('.container').style.maxHeight="calc(100vh - 65px)";
+  // document.querySelector('.container').style.maxHeight="calc(100vh - 65px)";
+
 }
+
+
+console.log(window.innerHeight)
+
+
+// function calcVH() {
+//   var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+//   document.getElementById("selector").setAttribute("style", "height:" + vH + "px;");
+// }
+// calcVH();
+// window.addEventListener('onorientationchange', calcVH, true);
+
+
+// PROBLEMS WITH CHROME FULL SCREEN NAV BAR!
