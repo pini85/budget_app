29f2473ce9462d000beb8f41527b097210428dd2
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-05-27 12:00:32 +0300

    g

--- src/scripts/controllers/statistics.js
+++ src/scripts/controllers/statistics.js
@@ -12,6 +12,6 @@ const isChromium = !!window.chrome;
 if(isChromium &&  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && document.querySelector('.container') ){
 
   console.log('hellooooo')
-  document.querySelector('.container').style.maxHeight="calc(100vh - 118px)";
+  document.querySelector('.container').style.maxHeight="calc(100vh - 114px)";
   document.body.style.position="fixed";
 }
