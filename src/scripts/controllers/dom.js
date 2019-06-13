c78fc949a88e0a0b5397860e1fda5cd47321dea7
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-06-13 12:19:07 +0300

    H

--- src/scripts/controllers/dom.js
+++ src/scripts/controllers/dom.js
@@ -60,3 +60,31 @@ document.onreadystatechange = function () {
     
   }
 }
+
+
+// Add the Web Share API
+
+if(document.body.className = "index") {
+  const shareButton = document.querySelector('.share-button');
+  const shareDialog = document.querySelector('.share-dialog');
+  const closeButton = document.querySelector('.close-button');
+  
+  shareButton.addEventListener('click', event => {
+    if (navigator.share) { 
+     navigator.share({
+        title: 'WebShare API Demo',
+        url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+      }).then(() => {
+        console.log('Thanks for sharing!');
+      })
+      .catch(console.error);
+      } else {
+          shareDialog.classList.add('is-open');
+      }
+  });
+  
+  closeButton.addEventListener('click', event => {
+    shareDialog.classList.remove('is-open');
+  });
+ 
+}
