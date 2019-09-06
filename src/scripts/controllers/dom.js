eeed64943d5049aa766b3900184fe8738909ccf0
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-09-06 14:44:44 +0300

    test

--- src/scripts/controllers/dom.js
+++ src/scripts/controllers/dom.js
@@ -53,25 +53,10 @@ if (document.querySelector('.navigation__container')) {
       const body = d.getElementsByTagName('body')[0];
       const bodyHeight = w.innerHeight || e.clientHeight || body.clientHeight;
       document.body.style.height = `${bodyHeight}px`;
-      if (!!window.chrome) {
         document.querySelector('.container__body').style.height=`calc(${bodyHeight}px - 58px)`;
         //disable scroll
-        document.addEventListener('touchstart', this.touchstart);
-        document.addEventListener('touchmove', this.touchmove);
-        
-        function touchstart(e) {
-            e.preventDefault()
-        }
-        
-        function touchmove(e) {
-            e.preventDefault()
-        }
-        touchstart();
-        touchmove();
-      }
-      console.log(bodyHeight);
     }
-  };
+  }
  
 
   // height: calc(100vh - 94px);
