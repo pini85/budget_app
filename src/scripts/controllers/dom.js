3e52cf1021a3db7b75b4bb578b35b5602951cb88
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-09-06 14:18:22 +0300

    test

--- src/scripts/controllers/dom.js
+++ src/scripts/controllers/dom.js
@@ -57,7 +57,7 @@ if (document.querySelector('.navigation__container')) {
         
       } 
       var isChromium = !!window.chrome;
-      document.querySelector('.container__body').style.height=`calc(${bodyHeight}px - 94px)`;
+      document.querySelector('.container__body').style.height=`calc(${bodyHeight}px - 58px)`;
       console.log(bodyHeight);
     }
   };
